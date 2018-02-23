6854975b7cc6485fe743d3204e4e0c28478af328
sdixit-dminc-com <sdixit@dminc.com>
2018-02-23 12:07:56 +0530

    Facetracker has been created

--- Kairos-SDK-Android-master/app/src/main/java/com/kairos/android/example/MainActivity.java
+++ Kairos-SDK-Android-master/app/src/main/java/com/kairos/android/example/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.gson.Gson;
 import com.kairos.Kairos;
 import com.kairos.KairosListener;
+import com.kairos.android.example.facetracker.FaceTrackerActivity;
 import com.rahul.media.main.MediaFactory;
 import com.rahul.media.model.Define;
 
@@ -164,7 +165,7 @@ public class MainActivity extends Activity implements KairosListener, View.OnCli
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.image_button:
-                startActivity(new Intent(this, EnrollmentActivity.class));
+                startActivity(new Intent(this, FaceTrackerActivity.class));
                 break;
             case R.id.recognise_button:
                 isRecogniseImage = true;
---
+++ Kairos-SDK-Android-master/app/src/main/java/com/kairos/android/example/Utils.java
@@ -0,0 +1,21 @@
+package com.kairos.android.example;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.ByteArrayInputStream;
+
+/**
+ * Created by Sdixit on 2/23/2018.
+ */
+
+public class Utils {
+
+    public static Bitmap convertToBitmap(byte[] byteArray)
+    {
+        ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(byteArray);
+        Bitmap bitmap = BitmapFactory.decodeStream(arrayInputStream);
+        return bitmap;
+    }
+
+}
