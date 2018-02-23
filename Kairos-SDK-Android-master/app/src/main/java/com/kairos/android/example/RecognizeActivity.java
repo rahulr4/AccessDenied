5352400af7b677d75d0ffc99aeac79750f47cdf8
Akashdeep Sachdeva <asachdeva@dminc.com>
2018-02-23 11:19:59 +0530

    Affdex sdk added

---
+++ Kairos-SDK-Android-master/app/src/main/java/com/kairos/android/example/RecognizeActivity.java
@@ -0,0 +1,11 @@
+package com.kairos.android.example;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2/23/2018.
+ */
+
+public class RecognizeActivity extends AppCompatActivity {
+
+}
